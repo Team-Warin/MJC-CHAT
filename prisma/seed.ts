a96e50c28bad2311b7f43e7c797a085f516f576c
kunhee <gh10043510@gmail.com>
2024-10-28 15:01:17 +0900

    ㅇ

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+// 이 코드는 /seed.json을 읽어서 데이터베이스에 초기 데이터를 넣는 코드입니다
+// 사용법 package.json 확인: npx prisma seed 
+import prisma from '@/lib/prisma';
+import seedJson from '@/../seed.json';
+
+interface SeedData {
+    chatRoom: { title: string; userId: number }[];
+    conversation: { sender: string; message: string; chatroomId: number; previousConversationId: number }[];
+    report: { conversationId: number; userId: number }[];
+}
+
+async function seedAll() {
+    const tables = Object.keys(seedJson) as (keyof SeedData)[];
+
+    for (const tableName of tables) {
+        const data = seedJson[tableName];
+
+        try {
+            if ((prisma as any)[tableName]) {
+                await (prisma as any)[tableName].createMany({ data });
+                console.log(`테이블 ${tableName}에 데이터 삽입 완료.`);
+            } else {
+                console.error(`❗ 테이블 ${tableName}이(가) Prisma 스키마에 존재하지 않습니다.`);
+            }
+        } catch (error) {
+            console.error(`❗ ${tableName} 데이터 삽입 중 오류 발생:`, error);
+        }
+    }
+}
+
+seedAll().catch(error => {
+    console.error('데이터 시드 삽입 중 오류 발생:', error);
+    process.exit(1);
+});
