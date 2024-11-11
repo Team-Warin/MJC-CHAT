e974b84015a75aeced3ec70d230417537bb91142
kunhee <gh10043510@gmail.com>
2024-11-11 16:21:32 +0900

    use ai-sdk framework

--- src/components/chat/chatwindow.tsx
+++ src/components/chat/chatwindow.tsx
@@ -13,6 +13,7 @@ import { motion } from 'framer-motion';
 import { usePathname, useParams } from 'next/navigation';
 
 import { createChatRoom } from '@/action/chatRoomHandler';
+import { askConversationStream } from '@/action/conversationHandler';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
@@ -25,7 +26,9 @@ import { LoginButton } from '@/components/navbar';
 
 import Conversation from '@/components/conversation';
 
-import { Conversation as ConversationType } from '@prisma/client'
+import { Conversation as ConversationPrisma } from '@prisma/client'
+
+import { useChat } from 'ai/react';
 
 export default function ChatWindow({
   session,
@@ -39,49 +42,13 @@ export default function ChatWindow({
   const pathname = usePathname();
   const { id: chatRoomId } = useParams();
 
-  const [conversations, setConversations] = useState<ConversationType[]>([]);
+  const [conversations, setConversations] = useState<ConversationPrisma[]>([]);
   const [value, setValue] = useState('');
 
-  useEffect(() => {
-    const fetchConversations = async () => {
-      try {
-        const response = await fetch(`/api/chatrooms/${chatRoomId}/conversations?page=1&limit=10`);
-        const data = await response.json();
-        console.log(data);
-        setConversations(data);
-      } catch (error) {
-        console.error('Failed to fetch messages:', error);
-      }
-    };
-    fetchConversations();
-  }, []);
-
-  const sendConversation = async () => {
-    if (value.trim().length === 0) return;
-
-    try {
-      const conversationTemp = {
-        id: 0,
-        sender: 'user',
-        message: value
-      };
-
-      setConversations([...conversations, conversationTemp as ConversationType]); // 유저 메시지 추가
-      setValue(''); // 입력창 초기화
-
-      const response = await fetch(`/api/chatrooms/${chatRoomId}/conversations`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ message: conversationTemp.message }),
-      });
-
-      const [, conversationAI] = await response.json();
-
-      setConversations(prevConversations => [...prevConversations, conversationAI]); // AI 메시지 추가
-    } catch (error) {
-      console.error('Failed to send message:', error);
-    }
-  };
+  const { messages, input, handleInputChange, handleSubmit, setData } = useChat({
+    api: `/api/chatrooms/${chatRoomId}/conversations`,
+    streamProtocol: 'text'
+  });
 
   return (
     <main className={style.chat_window}>
@@ -113,23 +80,36 @@ export default function ChatWindow({
         </div>
       </div>
       <div className={style.chat_window_body}>
-        {conversations.map(conversation => (
-          <Conversation userType={conversation.sender}>
-            <span>{conversation.message}</span>
+        {messages.map(message => (
+          <Conversation
+            userType={message.role === 'user' ? 'user' : 'ai'}
+            key={message.id}
+          >
+            <span>{message.content}</span>
           </Conversation>
         ))}
       </div>
       <div className={style.chat_window_footer}>
-        <div>
+        <form
+          onSubmit={e => {
+            setData(undefined);
+            handleSubmit(e);
+          }}
+        >
           <Textarea
-            value={value}
-            onValueChange={setValue}
-            onKeyDown={e => (e.key === 'Enter') && sendConversation()}
+            value={input}
+            onChange={handleInputChange}
             size='lg'
             variant='bordered'
             minRows={2}
             placeholder='자유롭게 대화해 보세요.'
           />
+          <Button
+            type='submit'
+            isIconOnly
+            radius='full'>
+            <FontAwesomeIcon size='sm' icon={faArrowRight} />
+          </Button>
           <motion.div
             initial={{ scale: 0 }}
             animate={{ scale: value.length > 0 ? 1 : 0 }}
@@ -137,13 +117,13 @@ export default function ChatWindow({
             className={style.send_button}
           >
             <Button
-              onClick={sendConversation}
+              type='submit'
               isIconOnly
               radius='full'>
               <FontAwesomeIcon size='sm' icon={faArrowRight} />
             </Button>
           </motion.div>
-        </div>
+        </form>
         <p>
           명전이는 부정확한 정보를 제공할 수 있으며, 이는 명지전문대학의 입장을
           대변하지 않습니다.
