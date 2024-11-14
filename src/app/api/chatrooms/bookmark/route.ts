cb3dba2a9808de0ffa08061ddc7f8b7e9e02c41c
HJ Lee <143615224+joonda@users.noreply.github.com>
2024-11-14 19:31:05 +0900

    [UPDATE] chatting room bookmark function

---
+++ src/app/api/chatrooms/bookmark/route.ts
@@ -0,0 +1,4 @@
+
+
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
--- src/components/conversation.tsx
+++ src/components/conversation.tsx
@@ -1,7 +1,10 @@
 import style from '@/styles/chat.module.css';
-
 import React from 'react';
 import Image from 'next/image';
+import { Button } from '@nextui-org/button';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as faBookmarkRegular, faComment } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Conversation({
   userType,
@@ -12,15 +15,36 @@ export default function Conversation({
 }) {
   /** AI 대화일 경우 */
   return (
-    <div className={style.paragraph} data-type={userType}>
-      <Image
-        src={userType === 'ai' ? '/avatar/mjc_face.webp' : '/avatar/user.webp'}
-        alt=''
-        width={27}
-        height={27}
-        className={style.icon}
-      />
-      {typeof children === 'string' ? <span>{children}</span> : children}
-    </div>
+    <>
+      <div className={style.paragraph} data-type={userType}>
+        <Image
+          src={userType === 'ai' ? '/avatar/mjc_face.webp' : '/avatar/user.webp'}
+          alt=''
+          width={27}
+          height={27}
+          className={style.icon}
+        />
+        {typeof children === 'string' ? <span>{children}</span> : children}
+      </div>
+      <div className={style.chat_icon}>
+        {userType === 'ai' && (
+          <>
+            <Button
+              isIconOnly
+              size='sm'
+              variant='light'
+              startContent={<FontAwesomeIcon icon={faComment} />}
+            />
+            <Button
+              isIconOnly
+              size='sm'
+              variant='light'
+              startContent={<FontAwesomeIcon icon={faBookmarkRegular} />}
+            />
+          </>
+        )}
+
+      </div>
+    </>
   );
 }
