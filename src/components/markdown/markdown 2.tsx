e7dde33109914c23aa470da60e6cdc28189c15fa
INIRU <iniru@kakao.com>
2024-11-21 22:10:50 +0900

    prompt add

--- src/components/markdown/markdown 2.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXRemoteProps } from 'next-mdx-remote/rsc';
-
-import { MDXRemote } from 'next-mdx-remote/rsc';
-
-const components = {};
-
-export default function Markdown(props: MDXRemoteProps) {
-  return <MDXRemote {...props} />;
-}
